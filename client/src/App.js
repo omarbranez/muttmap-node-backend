793de1fd759bef353e698f316fab4e2989befc16
Omar <omar.branez@gmail.com>
2022-01-19 01:21:50 -0500

    add react frontend

---
+++ client/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import './App.css';
+
+function App() {
+  
+  const [data, setData] = useState(null)
+  useEffect(()=>{
+    callBackendAPI()
+    .then(res => setData({ data: res.express }))
+    .catch(err => console.log(err))
+    }
+  )
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend')
+    const body = await response.json()
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{data}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+class App extends Component {
+state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import users from './routes/users_routes.js'
-// import files for initialization
+// import express from 'express'
+// import cors from 'cors'
+// import mongoose from 'mongoose'
+// import dotenv from 'dotenv'
+// import users from './routes/users_routes.js'
+// // import files for initialization
 
-dotenv.config()
+// dotenv.config()
 
-// create instance of express
+// // create instance of express
+// const app = express()
+
+// // use middleware
+// app.use(cors())
+// app.use(express.json())
+
+// // port information for mongodb
+// const CONNECTION_URL = process.env.MUTTMAP_DB_URL
+// const PORT = process.env.PORT
+
+// // use connect method with port info and options object to establish connection
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// // keep track of port being used
+//     .then(()=> app.listen(PORT, () => console.log(`listening on port ${PORT}`)))
+// // log error messages
+//     .catch((e) => console.log(e.message))
+
+// // set up initial root route
+// app.use('/', (req, res) => res.status(200).json({ message: "Hello World" }))
+// // set up users endpoints
+// app.use('/users', users)
+// // wildcard if route requested doesnt exist
+// app.use("*", (req, res) => res.status(404).json({ message: "Page Not Found" }))
+
+// export default app
+
+
+const express = require('express')
 const app = express()
+const port = 3000
 
-// use middleware
-app.use(cors())
-app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// port information for mongodb
-const CONNECTION_URL = process.env.MUTTMAP_DB_URL
-const PORT = process.env.PORT
-
-// use connect method with port info and options object to establish connection
-mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-// keep track of port being used
-    .then(()=> app.listen(PORT, () => console.log(`listening on port ${PORT}`)))
-// log error messages
-    .catch((e) => console.log(e.message))
-
-// set up initial root route
-app.use('/', (req, res) => res.status(200).json({ message: "Hello World" }))
-app.use('/users', users)
-app.use("*", (res, res) => res.status(404).json({ message: "Page Not Found" }))
-
-export default app
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

4f15348e56bca611c5329d0ef139682594588390
Omar <omar.branez@gmail.com>
2022-02-25 19:14:42 -0500

    create Report model

--- models/Report.js
+++ models/Report.js
@@ -1,6 +1,60 @@
 import mongoose from 'mongoose'
 const ReportSchema = new mongoose.Schema(
     {
-
-    }
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, "Must be associated with a user"]
+        },
+        breed: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Breed',
+            required: [true, "Must be associated with a breed"]
+        },
+        dogName: {
+            type: String,
+            required: true,
+            default: "Unknown",
+        },
+        dogColor: {
+            type: String,
+            required: true,
+            default: "Unknown",
+        },
+        dogAge: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        dogGender: {
+            type: String,
+            required: true,
+            default: "Unknown",
+        },
+        dogFeatures: {
+            type: String,
+            required: true,
+            default: "Not Provided",
+        },
+        dogDemeanor: {
+            type: String,
+            required: true,
+            default: "Not Provided"
+        },
+        reportLat: {
+            type: Number,
+            required: [true, "Please provide latitude"],
+        },
+        reportLng: {
+            type: Number,
+            required: [true, "Please provide longitude"],
+        },
+        reportShow: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
 )
+
+export default Report
